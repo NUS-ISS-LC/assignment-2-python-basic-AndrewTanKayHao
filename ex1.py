ebdae02323532fbb4b7b304a7582fd26c717c0ab
AndrewTanKayHao <e1674574@u.nus.edu>
2025-11-09 14:38:11 +0000

    added O(n) version of answer

--- ex1.py
+++ ex1.py
@@ -1,8 +1,17 @@
 def find(s, n):
 # write your implementation here
-# assume s is the number list [] and n is the target int 
-    for b in range(len(s)):
-        for c in range(b+1, len(s)):
-            if s[b]+s[c] == n:
-                return [b, c]        
+# O(n) assume s is the number list [] and n is the target int 
+    seenNumbersDictionary = {}
+    for i, num in enumerate(s):
+        complementNumber = n - num
+        if complementNumber in seenNumbersDictionary:
+            return [complementNumber, seenNumbersDictionary[complementNumber]]
+        seenNumbersDictionary[num] = i        
     return None
+
+# O(n2) assume s is the number list [] and n is the target int, returns indices
+#    for b in range(len(s)):
+#        for c in range(b+1, len(s)):
+#            if s[b]+s[c] == n:
+#                return [b, c]        
+#    return None
