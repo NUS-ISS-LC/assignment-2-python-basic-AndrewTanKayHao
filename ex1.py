d93ea3cca4f98d4d9548f5e51ba5fd253fe8a379
AndrewTanKayHao <e1674574@u.nus.edu>
2025-11-09 14:06:14 +0000

    added answer for ex1

--- ex1.py
+++ ex1.py
@@ -1,3 +1,8 @@
 def find(s, n):
 # write your implementation here
+# assume s is the number list [] and n is the target int 
+    for b in range(len(s)):
+        for c in range(b+1, len(s)):
+            if s[b]+s[c] == n:
+                return [b, c]        
     return None
